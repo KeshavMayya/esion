672c4d94bb817d3ca280db27acc8ab914ca7708d
granolja <48970070+granolja@users.noreply.github.com>
2019-05-14 08:14:10 -0400

    Update fft_test1.py

--- fft_test1.py
+++ fft_test1.py
@@ -52,31 +52,34 @@ nt = t[range(int(n/2))]
 #End seg#
 #########
 
-print(len(y))
-print(len(t))
 
-fig, ax = plt.subplots(5, 1)
-
-
-ax[1].plot(t,y)
-#ax[1].set_xlabel('Time')
-#ax[1].set_ylabel('Modulated Amplitude')
-
-ax[2].plot(frq,abs(Y), 'r') # plotting the spectrum
-#ax[2].set_xlabel('Modulated Freq (Hz)')
-#ax[2].set_ylabel('Modulated |Y(freq)|')
-
-ax[3].plot(frq, abs(Y-NM), 'r')
-#ax[3].set_xlabel('Freq (Hz)')
-#ax[3].set_ylabel('|Y(freq)|')
+if __name__ == '__main__':
+  print(len(y))
+  print(len(t))
 
 
-ax[4].plot(nt, (noisefixed*200))
-#ax[4].set_xlabel('Time')
-#ax[4].set_ylabel('Amplitude')
+  fig, ax = plt.subplots(5, 1)
 
-ax[0].plot(t,(np.cos(2*np.pi*ff*t)))
-#ax[0].set_xlabel('Original Time')
-#ax[0].set_ylabel('Original Amplitude')
 
-plot_url = py.plot_mpl(fig, filename='mpl-basic-fft')
+  ax[1].plot(t,y)
+  #ax[1].set_xlabel('Time')
+  #ax[1].set_ylabel('Modulated Amplitude')
+
+  ax[2].plot(frq,abs(Y), 'r') # plotting the spectrum
+  #ax[2].set_xlabel('Modulated Freq (Hz)')
+  #ax[2].set_ylabel('Modulated |Y(freq)|')
+
+  ax[3].plot(frq, abs(Y-NM), 'r')
+  #ax[3].set_xlabel('Freq (Hz)')
+  #ax[3].set_ylabel('|Y(freq)|')
+
+
+  ax[4].plot(nt, (noisefixed*200))
+  #ax[4].set_xlabel('Time')
+  #ax[4].set_ylabel('Amplitude')
+
+  ax[0].plot(t,(np.cos(2*np.pi*ff*t)))
+  #ax[0].set_xlabel('Original Time')
+  #ax[0].set_ylabel('Original Amplitude')
+
+  plot_url = py.plot_mpl(fig, filename='mpl-basic-fft')
